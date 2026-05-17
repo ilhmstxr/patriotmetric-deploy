1da28d23f7ac6e93bd5ae6490ff293173b97b106
Ragil Hidayatulloh <ragilhidayah1990@gmail.com>
2026-05-18 00:05:12 +0700

    feat: implement schema builders for company profile CMS pages

--- app/Filament/Pages/ComproForms/ProfileForm.php
+++ app/Filament/Pages/ComproForms/ProfileForm.php
@@ -5,9 +5,9 @@ namespace App\Filament\Pages\ComproForms;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 
 class ProfileForm
 {
